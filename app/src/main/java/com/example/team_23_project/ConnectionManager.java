f039fc46a014cf519ec5758b9702daf00311902b
abcaplan <c0034793@newcastle.ac.uk>
2021-12-12 16:08:43 +0000

    Add ConnectionManager java class with database details.

---
+++ app/src/main/java/com/example/team_23_project/ConnectionManager.java
@@ -0,0 +1,34 @@
+package com.example.team_23_project;
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionManager {
+    String username, password, ip, port, database;
+
+    public Connection connectionClass() {
+        ip = "cs-db.ncl.ac.uk";
+        database = "csc2033_team23";
+        username = "csc2033_team23";
+        password = "WhimHer84War";
+        port = "3306";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" +
+                    "database name=" + database + ";username=" + username + ";password=" + password + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        } catch (Exception e) {
+            Log.e("Error ", e.getMessage());
+        }
+        return connection;
+    }
+}
