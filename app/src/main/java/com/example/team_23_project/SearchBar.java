d7efb17e103619c7b9bc231ed26ca51b821ec123
louis <c0080358@newcastle.ac.co.uk>
2022-01-12 16:05:53 +0000

    Added a pop up window that shows if the user clicks on an unsupported university in the search bar

--- app/src/main/java/com/example/team_23_project/SearchBar.java
+++ app/src/main/java/com/example/team_23_project/SearchBar.java
@@ -1,12 +1,17 @@
 package com.example.team_23_project;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.SearchView;
 
 
@@ -39,7 +44,9 @@ public class SearchBar extends AppCompatActivity {
                                                                                    // String (e.g. will
                                                                                    // output 'Oxford'
                                                                                    // if user selects oxford)
-
+                    if (!selectedItem.equals("Newcastle")){
+                        popUpWindow(view);
+                    }
             }
         });
 
@@ -72,5 +79,32 @@ public class SearchBar extends AppCompatActivity {
 
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void popUpWindow(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 }
