397a02ebdeef9e61080085c843aea7cc10624061
louis <c0080358@newcastle.ac.co.uk>
2022-01-20 20:16:37 +0000

    Deleted activities and actions in nav_graph.xml as they are no longer needed

--- app/src/main/java/com/example/team_23_project/LoginActivity.java
+++ app/src/main/java/com/example/team_23_project/LoginActivity.java
@@ -89,7 +89,7 @@ public class LoginActivity extends AppCompatActivity {
 
                     } else {
 
-                        Toast.makeText(LoginActivity.this, "Login was successful",
+                        Toast.makeText(LoginActivity.this, "Login successful",
                                 Toast.LENGTH_SHORT).show();
 
                         Intent intent = new Intent( LoginActivity.this, HomePageActivity.class);
